6cd7c0301df69ad22418927c4269dea44c2742f7
lulwszabolcs <szabolcs050318@gmail.com>
2025-02-02 16:39:41 +0100

    Chat UI improvements

--- src/components/Support/SupportChat/SupportChat.js
+++ src/components/Support/SupportChat/SupportChat.js
@@ -1,5 +1,27 @@
+import styles from './SupportChat.module.css'
+import TextField from '@mui/material/TextField';
+
 export default function SupportChat() {
     return (
-        <h1 style={{textAlign:'center',marginTop:'10vh'}}>Chat</h1>
+        <div className={styles.flexbox}>
+        <h1 style={{marginTop:'10vh'}}>Chat</h1>
+
+        <div className={styles.chatmaincontainer}>
+            <div className={styles.messagecontainer}>
+                <div className={styles.usertextcontainer}>
+                    <p className={styles.usertext}>dasdasdasdasadsdsadas</p>
+                </div>
+                <div className={styles.systemtextcontainer}>
+                    <p className={styles.systemtext}>dasdasdasasdasddasdasdas</p>
+                </div>
+                <div className={styles.usertextcontainer}>
+                    <p className={styles.usertext}>dasdasdasdasadsdsadas</p>
+                </div>
+            </div>
+            <div className={styles.sendcontainer}>
+            <TextField id="outlined-multiline-static" multiline variant="outlined" className={styles.chatinput}  rows={2}/>
+            </div>
+        </div>
+        </div>
     )
 }
