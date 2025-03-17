1c8e80b59f6a5f22945bc5962f851bea7dab883b
Bazsa <tothfalusibalazs11@gmail.com>
2025-03-17 19:59:08 +0100

    fix: worker not found exception response code and body

--- airport_backend/src/main/java/com/example/airport/exception/WorkerExceptionHandler.java
+++ airport_backend/src/main/java/com/example/airport/exception/WorkerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.airport.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class WorkerExceptionHandler {
 
-    @ResponseStatus(value = HttpStatus.I_AM_A_TEAPOT, reason = "WORKER_NOT_FOUND")
     @ExceptionHandler(WorkerNotFoundException.class)
-    public void handleWorkerNotFound(WorkerNotFoundException ex) {}
+    public ResponseEntity<String> handleWorkerNotFound(WorkerNotFoundException ex) {
+        return new ResponseEntity<>("WORKER_NOT_FOUND", HttpStatus.I_AM_A_TEAPOT);
+    }
 }
