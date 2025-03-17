1f5962771f02e95cb8b0a1f347c781132e9c06a7
Bazsa <tothfalusibalazs11@gmail.com>
2025-03-17 21:01:51 +0100

    fix: Problem Not Found exception response code and body

--- airport_backend/src/main/java/com/example/airport/exception/ProblemExceptionHandler.java
+++ airport_backend/src/main/java/com/example/airport/exception/ProblemExceptionHandler.java
@@ -1,15 +1,15 @@
 package com.example.airport.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ProblemExceptionHandler {
-    @ResponseStatus(value = HttpStatus.I_AM_A_TEAPOT, reason = "PROBLEM_NOT_FOUND")
-    @ExceptionHandler(value = ProblemNotFoundException.class)
-    public void handleProblemNotFound(ProblemNotFoundException ex) {
-
+    @ExceptionHandler(ProblemNotFoundException.class)
+    public ResponseEntity<String> handleProblemNotFound(ProblemNotFoundException ex) {
+        return new ResponseEntity<>("PROBLEM_NOT_FOUND", HttpStatus.I_AM_A_TEAPOT);
     }
 }
