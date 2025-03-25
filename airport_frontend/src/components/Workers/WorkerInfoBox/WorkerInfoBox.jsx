a52ee6c2e1812ab1630bb6f472dfa884f5f9006e
lulwszabolcs <szabolcs050318@gmail.com>
2025-03-25 22:55:16 +0100

    Worker UI fix on large screen

--- airport_frontend/src/components/Workers/WorkerInfoBox/WorkerInfoBox.jsx
+++ airport_frontend/src/components/Workers/WorkerInfoBox/WorkerInfoBox.jsx
@@ -105,7 +105,7 @@ import { UserContext } from '../../../contexts/UserProvider';
               display: 'flex',
               flexDirection: 'row',
               flexWrap: 'wrap',
-              gap: '50px'
+              gap: isMobile? '50px':'4vw '
             }}>
               {filteredWorkers.map((worker)=>( 
                 <Card variant="outlined" data-testid={"workerCard"} style={{paddingTop:10,position:'relative',boxShadow:'5px 5px 5px 5px rgba(173, 216, 230, 0.616)',width:'15rem'}}>
